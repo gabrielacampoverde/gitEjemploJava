0265943c4ce7fe7fc4991d6a74e1ea593ec32cb8
gacampanaq <gacampanaq@gmail.com>
2024-01-13 22:08:45 -0500

    feature App

---
+++ src/main/java/com/exercise/Services/Main.java
@@ -0,0 +1,9 @@
+package com.exercise.Services;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 32;
+        int b = 20;
+        int c = a + b;
+    }
+}
---
+++ src/main/java/com/exercise/Services/ServiceAImpl.java
@@ -0,0 +1,4 @@
+package com.exercise.Services;
+
+public class ServiceAImpl {
+}
